372431b6cc37944db60f963adb0b71911be2bee7
divine-h4cker <56264743+divine-h4cker@users.noreply.github.com>
2019-10-27 23:28:28 +0600

    Add files via upload

--- kdv.py
+++ kdv.py
@@ -10,10 +10,10 @@ from lxml import html
 class PycoderSpider(scrapy.Spider):
 	name = "pycoder"
 	start_urls = [
-		'https://www.booking.com/searchresults.ru.html?label=gen173nr-1DCAQoggJCDWNpdHlfLTMyMTIyMTZIIVgEaIABiAEBmAEhuAEZyAEP2AED6AEB-AECiAIBqAIDuAKIwsjsBcACAQ&sid=7b7d32e62b0b16ad4055905d9054b0c3&tmpl=searchresults&city=-3212216&class_interval=1&dest_id=-3212216&dest_type=city&dtdisc=0&inac=0&index_postcard=0&label_click=undef&postcard=0&raw_dest_type=city&room1=A%2CA&sb_price_type=total&shw_aparth=1&slp_r_match=0&srpvid=7ad66dc45a180039&ss_all=0&ssb=empty&sshis=0&top_ufis=1&rows=25',
-
+		'https://www.booking.com/searchresults.ru.html?label=gen173nr-1FCAQoggJCDWNpdHlfLTMyMTIyMTZIIVgEaIABiAEBmAEhuAEZyAEM2AEB6AEB-AECiAIBqAIDuAKn54ztBcACAQ&sid=8b14cb8fba2ad81fbb135734e26620ae&sb=1&src=searchresults&src_elem=sb&error_url=https%3A%2F%2Fwww.booking.com%2Fsearchresults.ru.html%3Flabel%3Dgen173nr-1FCAQoggJCDWNpdHlfLTMyMTIyMTZIIVgEaIABiAEBmAEhuAEZyAEM2AEB6AEB-AECiAIBqAIDuAKn54ztBcACAQ%3Bsid%3D8b14cb8fba2ad81fbb135734e26620ae%3Btmpl%3Dsearchresults%3Bcity%3D-3212216%3Bclass_interval%3D1%3Bdest_id%3D-3212216%3Bdest_type%3Dcity%3Bdtdisc%3D0%3Binac%3D0%3Bindex_postcard%3D0%3Blabel_click%3Dundef%3Boffset%3D0%3Bpostcard%3D0%3Broom1%3DA%252CA%3Bsb_price_type%3Dtotal%3Bshw_aparth%3D1%3Bslp_r_match%3D0%3Bsrpvid%3D4f046553c05601b1%3Bss_all%3D0%3Bssb%3Dempty%3Bsshis%3D0%3Btop_ufis%3D1%26%3B&ss=Рига&is_ski_area=0&ssne=Рига&ssne_untouched=Рига&city=-3212216&checkin_year=&checkin_month=&checkout_year=&checkout_month=&group_adults=2&group_children=0&no_rooms=1&from_sf=1&rows=25'
 	]
 	visited_urls = []
+	offset = 0
 
 	def parse(self, response):
 			for post_link in response.xpath('//a[@class="hotel_name_link url"]/@href').getall():
@@ -22,19 +22,11 @@ class PycoderSpider(scrapy.Spider):
 				post_link = post_link.replace("\t","")
 				url = urljoin(response.url , post_link)
 				print("	JOINED TO HOTEL:" + url)
-				#yield response.follow(post_link,callback=self.parse)   
+				 
 				
 
-
-			
-			next_page_url = response.xpath("//a[@class='bui-pagination__link sr_pagination_link']/@href").extract()
-			if next_page_url:
-				next_href = next_page_url[-1]
-				next_page_url = next_href
-				next_page_url = next_page_url.replace("\r","")
-				next_page_url = next_page_url.replace("\n","")
-				next_page_url = next_page_url.replace("\t","")
-				next_page_url = urljoin(response.url +'/',next_href)
-				print("	NEXT_PAGE:"+next_page_url)
-				yield scrapy.Request(next_page_url,callback=self.parse)
-				
+			self.offset += 25
+			#&offset=
+			next_page_url = self.start_urls[0] + "&offset=" + str(self.offset) 
+			yield scrapy.Request(next_page_url,callback=self.parse)
+		
